0b20e9a7e95013efe3eabbdee7a5fd586ad4b3cc
Lieke van Son <aac.van.son@gmail.com>
2022-06-28 15:35:21 -0400

    last try local

--- src/scripts/TestZenodoData.py
+++ src/scripts/TestZenodoData.py
@@ -6,7 +6,26 @@ import matplotlib.pyplot as plt
 save_loc    =  '/Users/lieke/surfdrive/Documents'+'/SFRD_fit/src/tex/figures/' #/n/home04/lvanson/
 
 data_dir = 'src/data/vanSon21/'
-data = h5.File(data_dir + 'COMPAS_Output_wWeights.h5', 'r')
+
+
+
+
+# import tarfile
+
+# tar = tarfile.open(data_dir + 'COMPAS_Output_wRates.tar.gz', "r:gz")
+
+# for tarinfo in tar:
+#     print(tarinfo.name, "is", tarinfo.size, "bytes in size and is")
+#     if tarinfo.isreg():
+#         print("a regular file.")
+#     elif tarinfo.isdir():
+#         print("a directory.")
+#     else:
+#         print("something else.")
+# tar.close()
+
+
+data = h5.File(data_dir + 'COMPAS_Output_wRates.tar.gz', 'r')#'COMPAS_Output_wWeights.h5', 'r')
 
 print(data.keys())
 
@@ -17,3 +36,4 @@ data.close
 fix,ax = plt.subplots()
 ax.plot(np.linspace(0,1,10),np.linspace(0,1,10))
 plt.savefig(save_loc + 'testdata.jpg')
+
