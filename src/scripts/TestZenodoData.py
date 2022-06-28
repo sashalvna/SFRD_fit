143e08cad8db7a442ed6a125b00f43c55ae3abdd
Lieke van Son <aac.van.son@gmail.com>
2022-06-28 12:28:34 -0400

    further testing of zenodo data

--- src/scripts/TestZenodoData.py
+++ src/scripts/TestZenodoData.py
@@ -1,15 +1,16 @@
 # Trying to get Zenodo data to work
 import h5py as h5
 import numpy as np
+import matplotlib.pyplot as plt
 
 save_loc    =  '/Users/lieke/surfdrive/Documents'+'/SFRD_fit/src/tex/figures/' #/n/home04/lvanson/
 
-data_dir = 'src/data/vanSon21/COMPAS_Output_wRates/'
-data = h5.File(data_dir + 'COMPAS_Output_wWeights.h5', 'r')
+data_dir = 'src/data/vanSon21/'
+# data = h5.File(data_dir + 'COMPAS_Output_wWeights.h5', 'r')
 
-print(data.keys())
+# print(data.keys())
 
-data.close
+# data.close
 
 fix,ax = plt.subplots()
 ax.plot(np.linspace(0,1,10),np.linspace(0,1,10))
