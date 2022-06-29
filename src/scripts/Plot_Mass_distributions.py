2d85f2e15faf73798cd8db2d75424eea9b5a729c
LiekeVanSon <lieke_van_son@hotmail.com>
2022-06-28 20:36:20 -0400

    slowly working towards making it all self consistent

--- src/scripts/Plot_Mass_distributions.py
+++ src/scripts/Plot_Mass_distributions.py
@@ -33,7 +33,7 @@ import gc
 
 ######################################
 ## locations
-save_loc    =  '/Users/lieke/surfdrive/Documents'+'/SFRD_fit/src/tex/figures/' #/n/home04/lvanson/
+save_loc    =  'src/tex/figures/'#'/Users/lieke/surfdrive/Documents'+'/SFRD_fit/src/tex/figures/' #/n/home04/lvanson/
 data_dir    = '/Volumes/StorageSpac'+'/CompasOutput/v02.19.04/SFRD_fit_data/fWR1.0coolWind1.0/output/' # '/n/holystore01/LABS/hernquist_lab/Users/lvanson/'
 
 
@@ -69,212 +69,215 @@ def plot_mass_distribution(sim_dir = '', x_key = 'M_moreMassive', rate_keys = ['
                    xlabel = r'$M_{\mathrm{BH, 1}} \ \rm [M_{\odot}]$', ylabel = r'$\frac{d\mathcal{R}}{dM_{\mathrm{BH, 1} }} \ \mathrm{[Gpc^{-3}yr^{-1}M^{-1}_{\odot}]}$',
                    leg_args = {'loc':'lower left',  'bbox_to_anchor':[0.1, 0.], 'fontsize':20, 'title':''}, leg1_args = {'loc':'upper left', 'fontsize':18},
                    save_plot=False, save_name = 'Fiducial.png', multipanel = False, subplot = None):
-	"""
-	Read DCO, SYS and merger rate data, necesarry to make the plots in this 
+    """
+    Read DCO, SYS and merger rate data, necesarry to make the plots in this 
 
-	Args:
-	    sim_dir              --> [string] Location of data
+    Args:
+        sim_dir              --> [string] Location of data
 
-	Returns:
-	 plot
+    Returns:
+     plot
 
-	"""
+    """
 
-	#########################################
-	mass_binw = np.diff(bins)[0]
+    #########################################
+    mass_binw = np.diff(bins)[0]
 
-	plot_lines = []
-	leg_labels = []
+    plot_lines = []
+    leg_labels = []
 
-	#########################################
-	# Start plotting
-	if not multipanel: #(otherwise you define the Fig outside of this fucntion)
-		fig, ax = plt.subplots(figsize = (12, 10))
-	else:
-		ax = subplot
+    #########################################
+    # Start plotting
+    if not multipanel: #(otherwise you define the Fig outside of this fucntion)
+        fig, ax = plt.subplots(figsize = (12, 10))
+    else:
+        ax = subplot
 
-	################################################
-	# GWTC-3 Powerlaw + Peak Mass distribution
-	################################################ 
-	if plot_LIGO:
-		LIGO_data_dir = 'src/data/' #'/Volumes/StorageSpac/CompasOutput/output/'#'/n/home04/lvanson/LowMBH_peak/output/GWTC-3-population-data/analyses/PowerLawPeak/'
-		color_plpeak = 'grey'#'#1f78b4'
-		#################################################
-		## grab Powerlaw + Peak data from O3
-		#################################################
-		input_fname = LIGO_data_dir+'o3only_mass_c_iid_mag_iid_tilt_powerlaw_redshift_mass_data.h5'
-		##############################
-		# Calculate relevant values
-		mass_1 = np.linspace(2, 100, 1000)
-		mass_ratio = np.linspace(0.1, 1, 500)
-		with h5.File(input_fname, "r") as f:
-			mass_ppd = f["ppd"]
-			mass_lines = f["lines"]
-			mass_1_ppd = np.trapz(mass_ppd, mass_ratio, axis=0)
-			mass_1_lower = np.percentile(mass_lines["mass_1"], 5, axis=0)
-			mass_1_upper = np.percentile(mass_lines["mass_1"], 95, axis=0)
-		##############################
-		# plot the max posterior and the 95th percentile
-		ax.plot(mass_1, mass_1_ppd, lw=1.8, color=color_plpeak, zorder=1, label="$\mathrm{GWTC-3}$")
-		ax.fill_between(mass_1, mass_1_lower, mass_1_upper, alpha=0.14,color=color_plpeak,zorder=0)
+    ################################################
+    # GWTC-3 Powerlaw + Peak Mass distribution
+    ################################################ 
+    if plot_LIGO:
+        LIGO_data_dir = 'src/data/' #'/Volumes/StorageSpac/CompasOutput/output/'#'/n/home04/lvanson/LowMBH_peak/output/GWTC-3-population-data/analyses/PowerLawPeak/'
+        color_plpeak = 'grey'#'#1f78b4'
+        #################################################
+        ## grab Powerlaw + Peak data from O3
+        #################################################
+        input_fname = LIGO_data_dir+'o3only_mass_c_iid_mag_iid_tilt_powerlaw_redshift_mass_data.h5'
+        ##############################
+        # Calculate relevant values
+        mass_1 = np.linspace(2, 100, 1000)
+        mass_ratio = np.linspace(0.1, 1, 500)
+        with h5.File(input_fname, "r") as f:
+            mass_ppd = f["ppd"]
+            mass_lines = f["lines"]
+            mass_1_ppd = np.trapz(mass_ppd, mass_ratio, axis=0)
+            mass_1_lower = np.percentile(mass_lines["mass_1"], 5, axis=0)
+            mass_1_upper = np.percentile(mass_lines["mass_1"], 95, axis=0)
+        ##############################
+        # plot the max posterior and the 95th percentile
+        ax.plot(mass_1, mass_1_ppd, lw=1.8, color=color_plpeak, zorder=1, label="$\mathrm{GWTC-3}$")
+        ax.fill_between(mass_1, mass_1_lower, mass_1_upper, alpha=0.14,color=color_plpeak,zorder=0)
 
-		legend1 = plt.legend(**leg1_args)
+        legend1 = plt.legend(**leg1_args)
 
-	nplot = 0
-	    
-	################################################
-	# My Simulations
-	################################################
-	DCO = mfunc.read_data(loc = sim_dir +'/COMPAS_Output_wWeights.h5')
+    nplot = 0
 
-	# We'll show the change in rate between SFRD at several reference masses
-	m10, m25, m40 = [], [], []
+    ################################################
+    # My Simulations
+    ################################################
+    try:
+        DCO = mfunc.read_data(loc = sim_dir +'/COMPAS_Output_wWeights.h5')
+    except:
+        print('data not found')
 
-	print('nplot', nplot, '\n')
-	####################################################
-	### Loop over SFRD
-	for i, rate_key in enumerate(rate_keys):
-		print('rate_key', rate_key)
+    # We'll show the change in rate between SFRD at several reference masses
+    m10, m25, m40 = [], [], []
 
-		# ### ## Reading Rate data ##
-		try:
-			#     DCO_mask, redshifts, intrinsic_rate_density, intrinsic_rate_density_z0  = mfunc.read_rate_data(loc = sim_dir + '/Rate_info.hdf5', rate_key = rate_key)
-			with h5.File(sim_dir + '/Rate_info.hdf5' ,'r') as File:
-				redshifts                 = File['redshifts'][()]
-				# Different per rate key:
-				DCO_mask                  = File[rate_key]['DCOmask'][()] # Mask from DCO to merging systems  
-				#(contains filter for RLOF>CE and optimistic CE)
-				intrinsic_rate_density    = File[rate_key]['merger_rate'][()]
+    print('nplot', nplot, '\n')
+    ####################################################
+    ### Loop over SFRD
+    for i, rate_key in enumerate(rate_keys):
+        print('rate_key', rate_key)
 
-		except:
-			print('\n error reading', rate_key)
-			continue
-	    
-		# # # # # # # # # # # # # # # # # # 
-		#first bring it to the same shape as the rate table
-		merging_BBH    = DCO[DCO_mask]
-		#then apply the additional mask based on your prefs
-		merging_BBH         = merging_BBH[(merging_BBH['Stellar_Type@ZAMS(1)'] != 16)]
-		Red_intr_rate_dens  = intrinsic_rate_density[(DCO['Stellar_Type@ZAMS(1)'][DCO_mask] != 16), :]
+        # ### ## Reading Rate data ##
+        try:
+            #     DCO_mask, redshifts, intrinsic_rate_density, intrinsic_rate_density_z0  = mfunc.read_rate_data(loc = sim_dir + '/Rate_info.hdf5', rate_key = rate_key)
+            with h5.File(sim_dir + '/Rate_info.hdf5' ,'r') as File:
+                redshifts                 = File['redshifts'][()]
+                # Different per rate key:
+                DCO_mask                  = File[rate_key]['DCOmask'][()] # Mask from DCO to merging systems  
+                #(contains filter for RLOF>CE and optimistic CE)
+                intrinsic_rate_density    = File[rate_key]['merger_rate'][()]
 
-		# # # # # # # # # # # # # # # # # # 
-		## Calculate average rate density per z-bin
-		# crude_rate_density  = mfunc.get_crude_rate_density(Red_intr_rate_dens[:,:], redshifts, z_bin_edges)
-		#########################################
-		# X value and weight
-		x_vals              = merging_BBH[x_key]
-		i_redshift = np.where(redshifts == 0.2)[0][0]
-		print('i_redshift', i_redshift)
-		Weights             = Red_intr_rate_dens[:, i_redshift]#crude_rate_density[:,0]
-		print('!X!X!X!', np.shape(Weights), np.shape(x_vals) )
-		print(labels[i], ' len(table)=', len(merging_BBH) , ' Rate = ', np.sum(Weights), ' Gpc-3 yr-1')
+        except:
+            print('\n error reading', rate_key)
+            continue
+            
+        # # # # # # # # # # # # # # # # # # 
+        #first bring it to the same shape as the rate table
+        merging_BBH    = DCO[DCO_mask]
+        #then apply the additional mask based on your prefs
+        merging_BBH         = merging_BBH[(merging_BBH['Stellar_Type@ZAMS(1)'] != 16)]
+        Red_intr_rate_dens  = intrinsic_rate_density[(DCO['Stellar_Type@ZAMS(1)'][DCO_mask] != 16), :]
 
-		########################
-		# Get the Hist    
-		hist, bin_edge = np.histogram(x_vals, weights = Weights, bins=bins)
-		y_vals = hist/mass_binw
-		center_bins = (bin_edge[:-1] + bin_edge[1:])/2.
+        # # # # # # # # # # # # # # # # # # 
+        ## Calculate average rate density per z-bin
+        # crude_rate_density  = mfunc.get_crude_rate_density(Red_intr_rate_dens[:,:], redshifts, z_bin_edges)
+        #########################################
+        # X value and weight
+        x_vals              = merging_BBH[x_key]
+        i_redshift = np.where(redshifts == 0.2)[0][0]
+        print('i_redshift', i_redshift)
+        Weights             = Red_intr_rate_dens[:, i_redshift]#crude_rate_density[:,0]
+        print('!X!X!X!', np.shape(Weights), np.shape(x_vals) )
+        print(labels[i], ' len(table)=', len(merging_BBH) , ' Rate = ', np.sum(Weights), ' Gpc-3 yr-1')
 
-		# And the KDE
-		kernel = stats.gaussian_kde(x_vals, bw_method=kde_width, weights=Weights)
-		binwidth = np.diff(bin_edge)
+        ########################
+        # Get the Hist    
+        hist, bin_edge = np.histogram(x_vals, weights = Weights, bins=bins)
+        y_vals = hist/mass_binw
+        center_bins = (bin_edge[:-1] + bin_edge[1:])/2.
 
-		m10.append(kernel(10)*sum(hist)) # append value at reference mass 
-		m25.append(kernel(25)*sum(hist)) # append value at reference mass 
-		m40.append(kernel(40)*sum(hist)) # append value at reference mass 
+        # And the KDE
+        kernel = stats.gaussian_kde(x_vals, bw_method=kde_width, weights=Weights)
+        binwidth = np.diff(bin_edge)
 
-		########################
-		# Plot the Hist 
-		if show_hist:
-		    plot_lines.append(ax.step(center_bins, y_vals,  where='mid',label = None,#labels[i]+'$%s \mathrm{ \ Gpc^{-3} yr^{-1}}$'%(np.round(np.sum(Weights),1)) , 
-		                              alpha=1.0, lw = 3.5, zorder = i, color= Color, 
-		                              marker = 'o', markersize = 15) ) #edgecolor=color[i],
-		    # to prevent overflowing
-		    min_xkde = min(center_bins[y_vals>5e-4])
+        m10.append(kernel(10)*sum(hist)) # append value at reference mass 
+        m25.append(kernel(25)*sum(hist)) # append value at reference mass 
+        m40.append(kernel(40)*sum(hist)) # append value at reference mass 
 
-		########################
-		# Add KDE
-		if show_KDE:
-		    x_KDE = np.arange(0.1,50.,0.1)
-		    KDEy_vals =  kernel(x_KDE)*sum(hist) #re-normalize the KDE
-		    leg_labels.append(labels[nplot]+'$%s$'%(np.round(np.sum(Weights),1)))
+        ########################
+        # Plot the Hist 
+        if show_hist:
+            plot_lines.append(ax.step(center_bins, y_vals,  where='mid',label = None,#labels[i]+'$%s \mathrm{ \ Gpc^{-3} yr^{-1}}$'%(np.round(np.sum(Weights),1)) , 
+                                      alpha=1.0, lw = 3.5, zorder = i, color= Color, 
+                                      marker = 'o', markersize = 15) ) #edgecolor=color[i],
+            # to prevent overflowing
+            min_xkde = min(center_bins[y_vals>5e-4])
 
-		    if not show_hist:
-		        plot_lines.append(ax.plot(x_KDE, KDEy_vals, label = '', color=Color, lw= 5,  zorder =i+1,ls = linestyles[nplot]))
+        ########################
+        # Add KDE
+        if show_KDE:
+            x_KDE = np.arange(0.1,50.,0.1)
+            KDEy_vals =  kernel(x_KDE)*sum(hist) #re-normalize the KDE
+            leg_labels.append(labels[nplot]+'$%s$'%(np.round(np.sum(Weights),1)))
 
-		#     ########################
-		#     # Bootstrap   
-		#     if bootstrap:
-		#         indices = np.arange(len(x_vals))
-		#         hist_vals = np.zeros((bootstraps, len(x_KDE)))  #center_bins
-		#         for b in progressbar( range(len(hist_vals)), "Bootstrapping "+ labels[i] + ":"):
-		#             boot_index = np.random.choice(indices, size=len(indices), replace=True)
-		#             kernel         = stats.gaussian_kde(x_vals[boot_index], bw_method=kde_width, weights=Weights[boot_index])
-		#             Hist, _        = np.histogram(x_vals[boot_index], bins=bins, weights=Weights[boot_index],density=False)
-		#             hist_vals[b]   = kernel(x_KDE)*sum(Hist)
+            if not show_hist:
+                plot_lines.append(ax.plot(x_KDE, KDEy_vals, label = '', color=Color, lw= 5,  zorder =i+1,ls = linestyles[nplot]))
 
-		#         # calculate 1- and 2- sigma percentiles
-		#         y_vals = hist_vals#/mass_binw
+        #     ########################
+        #     # Bootstrap   
+        #     if bootstrap:
+        #         indices = np.arange(len(x_vals))
+        #         hist_vals = np.zeros((bootstraps, len(x_KDE)))  #center_bins
+        #         for b in progressbar( range(len(hist_vals)), "Bootstrapping "+ labels[i] + ":"):
+        #             boot_index = np.random.choice(indices, size=len(indices), replace=True)
+        #             kernel         = stats.gaussian_kde(x_vals[boot_index], bw_method=kde_width, weights=Weights[boot_index])
+        #             Hist, _        = np.histogram(x_vals[boot_index], bins=bins, weights=Weights[boot_index],density=False)
+        #             hist_vals[b]   = kernel(x_KDE)*sum(Hist)
 
-		#         percentiles = np.percentile(y_vals, [10., 90.], axis=0)
-		#         print('nplot',nplot, 'np.shape(percentiles)', np.shape(percentiles))
-		#         ax.fill_between(x_KDE, percentiles[0],percentiles[1], alpha=0.4, color=Color, zorder = 11) # 1-sigma
+        #         # calculate 1- and 2- sigma percentiles
+        #         y_vals = hist_vals#/mass_binw
 
-		nplot += 1
+        #         percentiles = np.percentile(y_vals, [10., 90.], axis=0)
+        #         print('nplot',nplot, 'np.shape(percentiles)', np.shape(percentiles))
+        #         ax.fill_between(x_KDE, percentiles[0],percentiles[1], alpha=0.4, color=Color, zorder = 11) # 1-sigma
+
+        nplot += 1
 
 
-	# #########################################
-	# # Show the variation in SFR at 3 different masses
-	# reference_masses = [10, 25, 40]
-	# for m, mpoint in enumerate([m10, m25, m40]):
-	# 	print('m', np.median(mpoint), max(mpoint), min(mpoint))
-	# 	print()
-	# 	ax.vlines(x=reference_masses[m], ymin=min(mpoint), ymax=max(mpoint), colors='k', lw=3, zorder = 20)
-	# 	ax.hlines(y=[min(mpoint), max(mpoint)], xmin=reference_masses[m]-0.5, xmax=reference_masses[m]+0.5, linewidth=3, color='k', zorder = 20)
-	# 	ax.text(reference_masses[m] - 0.7, (max(mpoint)+min(mpoint))/2 , r'%s $\times $'%(np.round( (max(mpoint)/min(mpoint))[0] , 1)) ,
-	# 		bbox=dict(facecolor='white', edgecolor='none', boxstyle='round,pad=0.1', alpha = 0.5), ha = 'right', size = 25, zorder = 20)
+#     #########################################
+#     # Show the variation in SFR at 3 different masses
+#     reference_masses = [10, 25, 40]
+#     for m, mpoint in enumerate([m10, m25, m40]):
+#         print('m', np.median(mpoint), max(mpoint), min(mpoint))
+#         print()
+#         ax.vlines(x=reference_masses[m], ymin=min(mpoint), ymax=max(mpoint), colors='k', lw=3, zorder = 20)
+#         ax.hlines(y=[min(mpoint), max(mpoint)], xmin=reference_masses[m]-0.5, xmax=reference_masses[m]+0.5, linewidth=3, color='k', zorder = 20)
+#         ax.text(reference_masses[m] - 0.7, (max(mpoint)+min(mpoint))/2 , r'%s $\times $'%(np.round( (max(mpoint)/min(mpoint))[0] , 1)) ,
+#             bbox=dict(facecolor='white', edgecolor='none', boxstyle='round,pad=0.1', alpha = 0.5), ha = 'right', size = 25, zorder = 20)
 
-	#########################################
-	# plot values
-	plt.text(0.70, 0.85, titletext, ha = 'center', transform=ax.transAxes, size = 25)
+    #########################################
+    # plot values
+    plt.text(0.70, 0.85, titletext, ha = 'center', transform=ax.transAxes, size = 25)
 
-	ax.set_xlim(x_lim)
-	ax.set_ylim(y_lim)
+    ax.set_xlim(x_lim)
+    ax.set_ylim(y_lim)
 
-	#####
-	# add legend for simulations
-	leg = ax.legend(**leg_args)
-	leg = plt.legend([l[0] for l in plot_lines], [l for l in leg_labels ],  **leg_args)
-	leg.set_zorder(102)
-	leg._legend_box.align = "right"
+    #####
+    # add legend for simulations
+    leg = ax.legend(**leg_args)
+    leg = plt.legend([l[0] for l in plot_lines], [l for l in leg_labels ],  **leg_args)
+    leg.set_zorder(102)
+    leg._legend_box.align = "right"
 
-	# Legend for GWTC-3
-	if plot_LIGO:
-		plt.gca().add_artist(legend1)
+    # Legend for GWTC-3
+    if plot_LIGO:
+        plt.gca().add_artist(legend1)
 
-	s = ['$[%s \leq z < %s]$'%(z_bin_edges[a],z_bin_edges[a+1]) for a in range(0,len(z_bin_edges)-1)]   
+    s = ['$[%s \leq z < %s]$'%(z_bin_edges[a],z_bin_edges[a+1]) for a in range(0,len(z_bin_edges)-1)]   
 
-	ax.set_xlabel(xlabel, fontsize = 30)
-	ax.set_ylabel(ylabel, fontsize = 30)
+    ax.set_xlabel(xlabel, fontsize = 30)
+    ax.set_ylabel(ylabel, fontsize = 30)
 
-	ax.set_yscale('log')
+    ax.set_yscale('log')
 
-	if not multipanel:
-		if save_plot:
-		    plt.savefig(save_loc+'/'+save_name , bbox_inches='tight')
+    if not multipanel:
+        if save_plot:
+            plt.savefig(save_loc+'/'+save_name , bbox_inches='tight')
 
-		plt.show()
-		# clear memory
-		gc.collect()
-	else:
-		return ax
+        plt.show()
+        # clear memory
+        gc.collect()
+    else:
+        return ax
 
 
 
 #################################################################################################
-#    																							#
-#       Call plots																				#	
-#																								#
+#                                                                                               #
+#                                          Call plots                                           #
+#                                                                                               #
 #################################################################################################
 
 fig = plt.figure( figsize = (24, 28))
