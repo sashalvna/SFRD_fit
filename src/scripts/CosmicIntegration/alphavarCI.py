e294794116773cfbb312b67a5131ec304687c582
Lieke van Son <aac.van.son@gmail.com>
2022-07-02 11:53:54 -0400

    making CI work with init func

---
+++ src/scripts/CosmicIntegration/alphavarCI.py
@@ -0,0 +1,30 @@
+"""
+Run a variation of the SFRD 
+(this is in a separate file so you dont have to rerun everything if one job fails)
+"""
+import numpy as np
+import os
+from subprocess import Popen, PIPE, call
+import subprocess
+import sys
+import paths
+
+import CallCosmicIntegration as CI
+
+if __name__ == "__main__": 
+	# Initialize values
+	CI.init()
+	                                         
+	#################################################################
+	# alpha variations
+	Call_Cosmic_Integration(CI.data_dir, CI.COMPASfilename, rCI.ate_file_name, 
+	                        ZdepSFRD_param_sets =[[[CI.mu0_best, CI.muz_best, CI.sigma0_best, CI.sigmaz_best, -0.9], CI.fid_sfr_parameters],
+	                                             [[CI.mu0_best, CI.muz_best, CI.sigma0_best, CI.sigmaz_best, -3.5], CI.fid_sfr_parameters]],
+	                        partitions = 'demink,conroy,hernquist,shared', Wtime = "1:00:00", mem = "120000")
+
+	import time
+	time.sleep(3000) # Sleep until the coscmic integration slurms should be done
+
+
+
+
