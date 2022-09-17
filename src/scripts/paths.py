c0a83e77722f4b6524457ddbedbb3d1eb21391c4
Lieke van Son <aac.van.son@gmail.com>
2022-09-17 17:45:00 -0400

    trying to solve the latex issue

--- src/scripts/paths.py
+++ src/scripts/paths.py
@@ -2,7 +2,10 @@
 Exposes common paths useful for manipulating datasets and generating figures.
 
 """
+import os
 from pathlib import Path
+
+os.environ["PATH"] += os.pathsep + str(Path.home() / "bin")
 
 # Absolute path to the top level of the repository
 root = Path(__file__).resolve().parents[2].absolute()
